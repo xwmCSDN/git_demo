13f4d18b0c4e12125413b28c5c34ef582c4eb607
xwm <3510569528@qq.com>
2022-07-27 18:30:12 +0800

    push commit

--- src/main/java/com/test/GitDemo01.java
+++ src/main/java/com/test/GitDemo01.java
@@ -7,5 +7,6 @@ public class GitDemo01 {
         System.out.println("git测试！3");
         System.out.println("git测试！hot-fix1");
         System.out.println("git测试！hot-fix2");
+        System.out.println("git测试！push hot-fix2");
     }
 }
