64e36d661d2676664d7b0e68068f317cf8133e58
xwm <3510569528@qq.com>
2022-07-27 18:11:11 +0800

    second commit

--- src/main/java/com/test/GitDemo01.java
+++ src/main/java/com/test/GitDemo01.java
@@ -2,6 +2,7 @@ package com.test;
 
 public class GitDemo01 {
     public static void main(String[] args) {
-        System.out.println("git测试！");
+        System.out.println("git测试！1");
+        System.out.println("git测试！2");
     }
 }
